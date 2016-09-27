72db2c6cb241260853e239b5faf65ae899c28671
Ralph Nathan <ralphnathan@google.com>
2016-09-27 16:56:05 -0700

    brcmfmac: Return time since boot when reporting scan results.
    The Android framework expects scan results to have the time since boot.
    This value is compared with the timestamp of the scan request in the
    function pollLatestScanData in
    com.android.server.wifi.scanner.SupplicantWifiScannerImpl. The framework
    only considers the scan results to be valid if the timestamp of the scan
    is greater than that of the scan request.
    
    BUG=31785785
    TEST=rpi3 wifi reassociates on reboot
    
    Change-Id: I8032a697f9cae42f605416abc55f96ff95b447fc

--- drivers/net/wireless/brcm80211/brcmfmac/cfg80211.c
+++ drivers/net/wireless/brcm80211/brcmfmac/cfg80211.c
@@ -2647,7 +2647,7 @@ static s32 brcmf_inform_single_bss(struct brcmf_cfg80211_info *cfg,
 	bss = cfg80211_inform_bss(wiphy, notify_channel,
 				  CFG80211_BSS_FTYPE_UNKNOWN,
 				  (const u8 *)bi->BSSID,
-				  0, notify_capability,
+				  ktime_get_boot_ns(), notify_capability,
 				  notify_interval, notify_ie,
 				  notify_ielen, notify_signal,
 				  GFP_KERNEL);
@@ -2754,10 +2754,10 @@ static s32 brcmf_inform_ibss(struct brcmf_cfg80211_info *cfg,
 	brcmf_dbg(CONN, "signal: %d\n", notify_signal);
 
 	bss = cfg80211_inform_bss(wiphy, notify_channel,
-				  CFG80211_BSS_FTYPE_UNKNOWN, bssid, 0,
-				  notify_capability, notify_interval,
-				  notify_ie, notify_ielen, notify_signal,
-				  GFP_KERNEL);
+				  CFG80211_BSS_FTYPE_UNKNOWN, bssid,
+				  ktime_get_boot_ns(), notify_capability,
+				  notify_interval, notify_ie, notify_ielen,
+				  notify_signal, GFP_KERNEL);
 
 	if (!bss) {
 		err = -ENOMEM;
